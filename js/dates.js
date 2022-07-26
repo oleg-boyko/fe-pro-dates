bec5edaed9ab7506bd4c0be92261016a4353684a
oleg-boyko <97238155+oleg-boyko@users.noreply.github.com>
2022-07-26 19:20:48 +0300

    Update dates.js

--- js/dates.js
+++ js/dates.js
@@ -24,16 +24,16 @@ function getDay(date, lang) {
     },
   };
   return dayNames[lang][date.getDay()];  
-}
-
+};
+function getDay(date, lang);
 
 // Принимает объект даты, и должно вернуть компоненты даты в виде строки.
 // Вид должен быть такой 12:02(часы и минуты), то есть если у вас одно число на одном из
 // компонентов, то добавляем 0 перед ним
 function formatTime(date) {
 date.toLocaleString();
-}
-
+};
+function formatTime(date);
 /*
 Напишите функцию getLastDayOfMonth(year, month), 
 возвращающую последнее число месяца. Иногда это 30, 31
@@ -46,8 +46,8 @@ month – месяц от 0 до 11.
 function getLastDayOfMonth(year, month) {
 let date = new Date(year, month + 1, 0);
   return date.getDate();
-}
-
+};
+function getLastDayOfMonth(year, month);
 
 module.exports = {
   getDay,
