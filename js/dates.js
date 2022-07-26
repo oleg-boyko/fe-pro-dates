9d79bf8e85f80383ea8c742ce724fc3f40c427d4
oleg-boyko <97238155+oleg-boyko@users.noreply.github.com>
2022-07-26 21:45:59 +0300

    Update dates.js

--- js/dates.js
+++ js/dates.js
@@ -14,7 +14,7 @@ function getLastDayOfMonth(year, month) {
 let date = new Date(year, month + 1, 0);
   return date.getDate();
 };
-function getLastDayOfMonth(year, month);
+function getLastDayOfMonth();
 
 module.exports = {
   getDay,
