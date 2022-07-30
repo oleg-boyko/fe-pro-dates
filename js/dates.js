0622a203aaeba260a242938e6ed5d8d1ae0f0491
oleg-boyko <97238155+oleg-boyko@users.noreply.github.com>
2022-07-30 21:13:18 +0300

    Update dates.js

--- js/dates.js
+++ js/dates.js
@@ -33,7 +33,7 @@ function getDay(date, lang) {
 // Вид должен быть такой 12:02(часы и минуты), то есть если у вас одно число на одном из
 // компонентов, то добавляем 0 перед ним
 function formatTime(date) {
-  if (date >= 0 && date <= 9) {
+  if (date >= 0 && date <= 24) {
 		return '0' + date;
 	} else {
 		return date.toLocaleTimeString();;
