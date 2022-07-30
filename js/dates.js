78f1cb37c82f063269d3b81b3f218f2a7aa67d6e
oleg-boyko <97238155+oleg-boyko@users.noreply.github.com>
2022-07-30 22:27:34 +0300

    Update dates.js

--- js/dates.js
+++ js/dates.js
@@ -34,7 +34,7 @@ function getDay(date, lang) {
 // компонентов, то добавляем 0 перед ним
 function formatTime(date) {
    //let s = date.toTimeString();
-  return date.toLocaleTimeString();
+  return date.toTimeString();
 };
 
 
